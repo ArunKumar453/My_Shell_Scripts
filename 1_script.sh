1fe4dbddfd2d3942c7a2540b7ecb9a4b107f4e13
ArunKumar453 <118875836+ArunKumar453@users.noreply.github.com>
2023-09-14 12:38:18 +0530

    Update 1_script.sh

--- 1_script.sh
+++ 1_script.sh
@@ -1,5 +1,16 @@
 #shebang
+#why do we have to use shebang in bash
+# Ans:- To specify the correct shell to use for a shell script. If a Shebang is not specified, the system uses the default interpreter belonging to the current shell.
+#echo :- Sending output to the terminal
+
+#How to run 
+
+
+
+#Scripts starts from here
+
 #!/bin/bash
 
 echo "Welcome to Bash Scripting"
+
 
