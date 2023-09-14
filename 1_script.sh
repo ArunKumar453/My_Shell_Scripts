d771844c5037606a2568aa96189754458e37baf9
ArunKumar453 <118875836+ArunKumar453@users.noreply.github.com>
2023-09-14 13:03:28 +0530

    Update 1_script.sh

--- 1_script.sh
+++ 1_script.sh
@@ -1,9 +1,35 @@
+<<comment 
 #shebang
+
 #why do we have to use shebang in bash
 # Ans:- To specify the correct shell to use for a shell script. If a Shebang is not specified, the system uses the default interpreter belonging to the current shell.
 #echo :- Sending output to the terminal
 
-#How to run 
+#How to run a script
+# Make sure script has execute permission rwx
+# chmod 744 script_name.sh
+
+#Ans:- ./script.sh(Both relative or absolute path)
+# bash script_name.sh 
+# Relative path :- path related to the present working directory
+# Absolute Path :-Path from the direct directory
+
+# control + C :- To terminate 
+# Control + Z :- To Stop
+
+
+
+
+
+
+
+
+
+
+
+comment 
+
+
 
 
 
