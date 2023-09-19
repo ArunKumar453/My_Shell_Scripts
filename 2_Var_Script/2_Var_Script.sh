1e344b0d69adf35f1522117ca8f9ec015efefb4d
ArunKumar453 <118875836+ArunKumar453@users.noreply.github.com>
2023-09-19 06:53:03 +0530

    Create 2_Var_Script.sh

---
+++ 2_Var_Script/2_Var_Script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Script to show how to use variables
+
+#Variables are basically temporary storage in the memory
+
+
+
+age=10
+Name="Arun"
+
+echo "My name is  $Name and age is $age"
+
+$Name
+#Name is variable
+# Dollor sign extracts the value from variable
+
+
+#Variable to store the output of command
+
+Hostname=$(hostname)
+
+echo "name of the machine is $Hostname"
+
+
+#Constant Variable
+#Once you defined the varible  and dont want to change it until the end of script 
+
+readonly var_name=value
+readonly college="DSCE"
+
+echo "My college is $college"
