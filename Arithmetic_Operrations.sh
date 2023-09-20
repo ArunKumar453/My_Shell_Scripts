5454593ad0fe095bdb46a78e4ed25d4260dd3452
ArunKumar453 <118875836+ArunKumar453@users.noreply.github.com>
2023-09-20 12:51:21 +0530

    Arithmetic_Operrations.sh

---
+++ Arithmetic_Operrations.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+
+
+read -p "enter the number" num
+
+
+if [[ $num -eq 100 ]]
+then
+echo "equal"
+else
+echo "Not equal"
+fi
+
+
+if [[ $num -ge 100 ]]
+then
+echo "greater or equal to"
+else
+echo "Not qula to greater than"
+fi
+
+
+if [[ $num -le 70 ]]
+
+then
+echo "less than"
+else
+echo "Greater than"
+
+fi
+
+
+
+
+if [[ $num -ne 100 ]]
+then
+echo "Not equal"
+else
+echo "equal"
+fi
+
+
+if [[ $num -gt 100 ]]
+then
+echo "greater"
+else
+echo "ok"
+fi
+
+
+if [[ $num -lt 1000 ]]
+then
+echo "what"
+else
+echo "Who"
+fi
+
+
+
+
