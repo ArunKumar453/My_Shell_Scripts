b665551bac5537cea4102bc6dfd40a0545267186
ArunKumar453 <118875836+ArunKumar453@users.noreply.github.com>
2023-09-21 12:24:13 +0530

    Connectivity_check_Script.sh

---
+++ Connectivity_check_Script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+read -p  "which site you want to check?" site
+
+ping -c 1 $site
+sleep 5s
+
+
+if [[ $? -eq 0 ]]
+then
+echo "successfully connected to $site"
+else
+echo "unbale to connect to $site"
+fi
+
